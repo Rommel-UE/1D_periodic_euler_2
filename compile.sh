7f568c37409d85305be8e95d574097ed24162de1
Rommel-UE <rommelin82@gmail.com>
2024-07-02 01:28:22 -0500

    Update compile.sh
    incluir la inicialización y finalización del entorno MPI y comentarios para indicar la ejecución de las aplicaciones MPI `double.p` y `single.p`. Además, se han agregado líneas para ejecutar las aplicaciones MPI utilizando `mpirun` con el número de procesos deseado y los argumentos necesarios `<num_points>` y `<wave_number>`.

--- compile.sh
+++ compile.sh
@@ -7,6 +7,16 @@ rk4.cpp \
 FluxFunctions.cpp \
 RHSoperator.cpp"
 
+# Compilación de la aplicación double.p
 mpic++ -g -O3 -pg -D_DOUBLE_ $SOURCE_FILES -Iincludes -o double.p
 
+# Compilación de la aplicación single.p
 mpic++ -g -O3 -pg $SOURCE_FILES -Iincludes -o single.p
+
+# Ejecución de la aplicación double.p
+echo "Running double.p with MPI"
+mpirun -np <num_procesos> ./double.p <num_points> <wave_number>
+
+# Ejecución de la aplicación single.p
+echo "Running single.p with MPI"
+mpirun -np <num_procesos> ./single.p <num_points> <wave_number>
